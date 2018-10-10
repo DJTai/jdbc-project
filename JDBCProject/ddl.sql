8dea42c398d10acd666010ada3053986e65f246e
David T <djtai@protonmail.com>
2018-10-10 09:57:17 -0700

    Add DDL for BOOKS table

--- JDBCProject/ddl.sql
+++ JDBCProject/ddl.sql
@@ -18,3 +18,21 @@ CREATE TABLE writingGroups(
     CONSTRAINT writingGroups_pk
         PRIMARY KEY(groupName)
 );
+
+-- TODO: Description of Books table
+CREATE TABLE books(
+    groupName VARCHAR(30) NOT NULL,
+    bookTitle VARCHAR(120) NOT NULL,
+    publisherName VARCHAR(40) NOT NULL,
+    yearPublished INTEGER NOT NULL,
+    numberPages INTEGER NOT NULL,
+    CONSTRAINT books_pk
+        PRIMARY KEY(groupName, bookTitle),
+    CONSTRAINT books_ck
+        UNIQUE (bookTitle, publisherName),
+    CONSTRAINT books_fk01
+        FOREIGN KEY (groupName) REFERENCES writingGroups(groupName),
+    CONSTRAINT books_fk02
+        FOREIGN KEY (publisherName) REFERENCES publishers(publisherName)
+);
+    

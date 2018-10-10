6a991a185cd397a162118478afe7081bea50349a
Chelsea Marfil <chelseamarfil@yahoo.com>
2018-10-10 09:09:04 -0700

    Created DDL for publishers table.

--- JDBCProject/ddl.sql
+++ JDBCProject/ddl.sql
@@ -1,1 +1,10 @@
--- Start here
+-- Table publishers holds a publisher's name, their address, their phone number, and their email address.
+-- 
+CREATE TABLE publishers (
+	publishername VARCHAR(40) NOT NULL,
+	publisheraddress VARCHAR(50) NOT NULL,
+	publisherphone VARCHAR(20) NOT NULL,
+	publisheremail VARCHAR(40) NOT NULL,
+	CONSTRAINT publishers_pk
+	PRIMARY KEY(publishername));
+	

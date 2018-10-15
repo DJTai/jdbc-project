b708ad83a6488c67d2d88d51e2f85397f5a01269
David T <djtai@protonmail.com>
2018-10-15 10:49:11 -0700

    Start menuPrompts() method (mostly skeleton code)

--- JDBCProject/src/jdbcproject/JDBCProject.java
+++ JDBCProject/src/jdbcproject/JDBCProject.java
@@ -28,6 +28,7 @@ public class JDBCProject {
       strings, but that won't always be the case.
      */
     static final String DISPLAY_FORMAT = "%-5s%-15s%-15s%-15s\n";
+    static final String DISPLAY_WRITING_GROUPS = "%-30s%-30s%-6s%-20s\n";
 
     /* JDBC driver name and DB URL */
     static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";
@@ -85,6 +86,7 @@ public class JDBCProject {
             // STEP 4: Execute a query
             System.out.println("Creating a statement...");
             statement = connection.createStatement();
+            
             String sql;
             ResultSet resultSet;
 //            sql = "SELECT au_id, au_fname, au_lname, phone FROM Authors";
@@ -138,4 +140,91 @@ public class JDBCProject {
             }
         }
     }
+
+    /**
+     *
+     * @param mConnection
+     * @param mStatement
+     */
+    public static void menuPrompts(Connection mConnection, Statement mStatement) {
+        Scanner stdin = new Scanner(System.in);
+        int response = -1;
+
+        // Keep looping until user exits
+        while (response != 4) {
+            System.out.println("What are we searching for?");
+            System.out.println("1. Writing Groups");
+            System.out.println("2. Publishers");
+            System.out.println("3. Books");
+            System.out.println("4. Exit");
+            System.out.print("Choice: ");
+
+            response = stdin.nextInt(); // NOTE: If asking for int then a String, use stdin.nextLine() after
+
+            /* Show different submenus based off user reponse */
+            switch (response) {
+                case 1:
+                    /* Writing Groups Submenu */
+                    stdin.nextLine();  // Clear any extra user input
+                    int wgResponse;    // Response for Writing group submenu
+
+                    System.out.println("Searching for Writing Groups:");
+                    System.out.println("1. All");
+                    System.out.println("2. By Name");
+                    System.out.println("3. Main Menu");
+
+                    wgResponse = stdin.nextInt();
+
+                    // Writing Group inner-menu
+                    switch (wgResponse) {
+                        case 1:
+                            // TODO: Call Chelsea's method
+                            break;
+
+                        case 2:
+                            // TODO: List by user input                            
+                            break;
+
+                        case 3:
+                            // TODO: Return to main menu
+                            break;
+
+                        default:
+                            // TODO: Return to main menu
+                            break;
+                    }
+
+                    break;  // end case 1
+
+                case 2:
+                    /* Publishers Submenu */
+                    stdin.nextLine();  // Clear any extra input
+                    int pubResponse;
+                    
+                    break;  // end case 2
+
+                case 3:
+                    /* Books Submenu */
+                    stdin.nextLine();  // Clear any extra input
+                    int bkResponse;
+                    
+                    break;
+
+                case 4:
+                    /* Exit Program */
+                    stdin.nextLine();  // Clear any extra input
+                    System.out.println("Thank you for using our services. Farewell!");
+                    break;
+
+                default:
+                    // TODO: Reprompt menu
+                    stdin.nextLine();  // Clear any extra input
+                    break;
+            }
+            // end switch(response)
+        }
+        // end while-loop
+    }
+    // end method
+    
 }
